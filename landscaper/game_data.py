c7fc601899dabc6ad8bb2d6e9b87bf2f51356271
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-05-31 17:56:03 -0400

    landscaper init, added starting data

---
+++ landscaper/game_data.py
@@ -0,0 +1,8 @@
+from tools import tools
+
+class Game:
+    def __init__(self):
+        self.running = True
+        self.win_amount = 10
+        self.tools = tools
+        # self.new_game()
---
+++ landscaper/tools.py
@@ -0,0 +1,22 @@
+tools = {
+    "rusty scissors": {
+        "cost": 5,
+        "earnings": 5,
+        "owned": 0
+    },
+    "push lawnmower": {
+        "cost": 25,
+        "earnings": 50,
+        "owned": 0
+    },
+    "battery-powered lawnmower": {
+        "cost": 250,
+        "earnings": 100,
+        "owned": 0
+    },
+    "team of starving students": {
+        "cost": 500,
+        "earnings": 250,
+        "owned": 0
+    }
+}
