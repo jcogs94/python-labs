f89ecdfd650e22c2d43e67518dd6217ea1b741fa
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-05-31 11:31:40 -0400

    separated out to mult files, data

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,34 +1,14 @@
 import os
-import math
+import print_game
+from words import words
 
-def heading_str():
-    line_len = 60
-    lines = '=' * line_len
-
-    title = 'HANGMAN'
-    num_side_equals = line_len - len(title) - 2
-    
-    left_side_equals = ''
-    right_side_equals = ''
-    if num_side_equals % 2 == 0:
-        left_side_equals = '=' * (num_side_equals / 2)
-        right_side_equals = left_side_equals
-    else:
-        left_side_equals = '=' * math.ceil(num_side_equals / 2)
-        right_side_equals = '=' * math.floor(num_side_equals / 2)
-    
-    title = left_side_equals + ' ' + title + ' ' + right_side_equals
-
-    print(lines)
-    print(title)
-    print(lines)
 
 game_running = True
-
 while game_running:
     os.system('clear')
-    heading_str()
-    print('New game...')
+    print_game.heading()
+    print_game.state()
+
     user_select = input('Enter a letter (q to quit): ')
 
     if user_select == 'q':
