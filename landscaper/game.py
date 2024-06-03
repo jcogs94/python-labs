ff4e69b96e1e2c98badeedc2506beac1bec3d664
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-06-03 09:15:05 -0400

    game loop better defined for user input

--- landscaper/game.py
+++ landscaper/game.py
@@ -1,8 +1,22 @@
 from game_data import Game
 from display.display_game import display_game
 
+def get_input(prompt):
+    print()
+    user_input = input(prompt)
+    return user_input
+
 game = Game()
 display_game(game)
 
 while game.running:
-    game.running = False
+    user_input = get_input('What would you like to do? ')
+
+    if user_input == "1":
+        print('store...')
+    elif user_input == "2":
+        print('Cutting grass...')
+    elif user_input == "3":
+        game.running = False
+    else:
+        print('Invalid input. Try again.')
